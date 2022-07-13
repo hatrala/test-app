996bc200baa25fe5ec2aecbcdf98bd4b0b730b16
hatrala <hatralaz2001@gmail.com>
2022-07-13 11:47:48 +0700

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,12 @@
-
-
-export const ApiUrl = "http://localhost:3000"
+export const ApiUrl = "http://localhost:3000";
 
 export const User = {
-    id : '',
-    username : '',
-    email : '',
-    gender : '',
-    type : ''
-}
+  id: "",
+  username: "",
+  email: "",
+  gender: "",
+  type: "",
+};
+
+export const AccessToken = "AccessToken";
+
